fc9b56d052af5a44192751ab39734ea4fa431fed
Tomislav Dukez <tomdu3@ymail.com>
2024-04-03 07:59:56 +0100

    Add words_import function

--- project.py
+++ project.py
@@ -1,1 +1,16 @@
 import random
+
+def words_import(filename):
+    '''
+    Imports the words list from a file
+    words taken from the website https://www.hangmanwords.com/words
+
+    '''
+    with open(filename, 'r') as file:
+        words = file.readlines()
+    return [word.strip() for word in words]
+
+
+WORDS = words_import('words.txt')
+
+print(WORDS)
