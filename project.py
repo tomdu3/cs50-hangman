e0b66d899f45e14837988f205bbbfdc7c0b39a5a
Tomislav Dukez <tomdu3@ymail.com>
2024-04-03 08:05:51 +0100

    Add hangman_import function and test

--- project.py
+++ project.py
@@ -13,4 +13,22 @@ def words_import(filename):
 
 WORDS = words_import('words.txt')
 
-print(WORDS)
+def hangman_import():
+    '''
+    Imports hangman ASCI art that was taken from
+    website https://inventwithpython.com/bigbookpython/project34.html
+    '''
+
+    hangman = {}
+    with open('hangman.txt') as f:
+        first = f.read().split('()\n')
+
+    for index, value in enumerate(first):
+        hangman[len(first)-index-1] = value
+
+    return {key:value for key, value in hangman.items()}
+
+HANGMAN = hangman_import()
+
+for value in HANGMAN.values():
+    print(value)
